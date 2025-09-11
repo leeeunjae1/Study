6ca1a91213a888894542f27b70f345adad9bb649
이은재 <sinbob98@gmail.com>
2025-09-12 06:07:09 +0900

    예외 처리 단계 별 실습

---
+++ java-mid1/src/exception/ex2/MainV2.java
@@ -0,0 +1,28 @@
+package exception.ex2;
+
+import java.util.Scanner;
+
+public class MainV2 {
+
+    public static void main(String[] args) throws NetworkClientExceptionV2 {
+        // NetworkServiceV1_1 networkService = new NetworkServiceV1_1();
+        // NetworkServiceV1_2 networkService = new NetworkServiceV1_2();
+        // NetworkServiceV2_1 networkService = new NetworkServiceV2_1();
+        // NetworkServiceV2_2 networkService = new NetworkServiceV2_2();
+        // NetworkServiceV2_3 networkService = new NetworkServiceV2_3();
+        // NetworkServiceV2_4 networkService = new NetworkServiceV2_4();
+        NetworkServiceV2_5 networkService = new NetworkServiceV2_5();
+
+        Scanner in = new Scanner(System.in);
+        while (true) {
+            System.out.print("전송할 문자: ");
+            String input = in.nextLine();
+            if (input.equals("exit")) {
+                break;
+            }
+            networkService.sendMessage(input);
+            System.out.println();
+        }
+        System.out.println("프로그램을 정상 종료합니다.");
+    }
+}
